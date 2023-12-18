47658fc10b02d3e3ca21c13cbc5a03f457645511
ijlalansari1122 <ansariijlal90@gmail.com>
2023-12-18 21:54:06 +0500

    some list code added

--- Day 3/multipleifs.py
+++ Day 3/multipleifs.py
@@ -1,17 +1,61 @@
-#  this code will be used for multiple ifs
-import calendar
+# #  This code will be functions , loops and statements.
+# # This code will solve some problem statements
+#
+# import calendar
+#
+# # function for checking leap year
+# def checkleap(year):
+#
+#    yearcheck = calendar.isleap(year)
+#    return yearcheck
+# # using the function to test it
+#
+#
+# Checkyear= int(input('please enter the year'))
+# #
+# if  checkleap(Checkyear) :
+#     print(f'correct the year is leapyear')
+# #
+# else:
+#     print('sorry the year is not leapyear')
+# #
+#
+# # function for band name generator
+#
+# def Bandnamegen_(first_n , second_n):
+#   bandname = first_n + second_n
+#   return bandname;
+#
+#
+# # using function for implementing function
+#
+# F_name = input('please enter the first name ')
+# S_name = input('please enter second name generator ')
+#
+# Bandnamegenerator = Bandnamegen_(F_name , S_name);
+#
+# if  Bandnamegenerator =='ijlalansari':
+#     print('thus the name is being searched')
+#
+#
+# elif Bandnamegenerator == 'prophetmuhammad' or Bandnamegenerator == 'maulaali':
+#     print('mashallah')
+#
+# else: print('please enter a valid name')
+#
+#
+#
+#
+# sorting bubble
+data = [22,1,3,5,6,8,4,3323,232]
+#
+
+def random(x=data ,y=data):
+    x1 = max(x)
+    y2 = min(y)
+    return x1, y2
 
 
-def checkleap(year):
+called =random()
 
-   yearcheck = calendar.isleap(year)
-   return yearcheck
-
-
-Checkyear= int(input('please enter the year'))
-
-if  checkleap(Checkyear) :
-    print('correct the year is leapyear')
-
-else:
-    print('sorry the year is not leapyear')
+print(f'the maximum number is  {called}')
