17014bb6038e1f7dd5a2b0f57cddc91304a7325f
ijlalansari1122 <ansariijlal90@gmail.com>
2024-08-29 15:59:35 +0500

    hangman project completed

--- Day-7/Day7.py
+++ Day-7/Day7.py
@@ -1,35 +1,62 @@
+#Step 4
+
 import random
+from hangman_words import word_list
+from hangman_logo import stages , logo
 
-#Step 1 
 
 
-word_list = ["aardvark", "baboon", "camel"]
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+end_of_game = False
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+#Set 'lives' to equal 6.
+lives = 6 
 
 
+#Testing code
+print(logo)
+print(f'Pssst, the solution is {chosen_word}.')
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+#Create blanks
+display = []
+for _ in range(word_length):
+    display.append("_")
 
-words =['_']*len(chosen_word)
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    if guess in display:
+           print(f"you have already guessed  {guess}")
 
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+      
+#TODO-2: - If guess is not a letter in the chosen_word
+# #Then reduce 'lives' by 1. 
+    if guess not in chosen_word:
+              print(f"you have guessed {guess} , which is not in the word")
+                  
+              lives -=1
+              print(stages[lives])
 
-while True:
+              if lives == 0:
+                         
+               end_of_game = True            
+               print("You lose")  
+               break     
+    #If lives goes down to 0 then the game should stop and it should print "You lose."
 
- guess = input("Hello , can you assign a letter for the hangman game ").lower()
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+    #Join all the elements in the list and turn it into a String.
+    print(f"{ "".join(display)}")
 
- lives = 5
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
 
- if guess == chosen_word:
-    
-    
-    print("Right")
-    
-    
-    
-    
- else:
-    print("Wrong")
-    lives=-1
+                  
